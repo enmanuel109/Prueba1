03c6de6bc554dcd11d7df1ca76eab1842b4f4de8
enmanuel109 <enmanuelcantarer001@gmail.com>
2025-04-25 00:30:05 -0600

    carga inicial

---
+++ src/prueba/practica.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba;
+
+/**
+ *
+ * @author Cantarero
+ */
+public class practica {
+     public static void main(String[] args){
+        /*
+        tema: Variables
+        sintaxis:
+        Tipo Datos Identificador;
+        Entrada de datos en asignar a la variable:
+        1- Valor Constante.
+        2- valor Variante(desde el teclado)
+        */
+        //Datos de Alumno
+        String nombre_Alumno="Elkin";
+        int edadAlumno=18;
+        double notafinal=64.4;
+        char genero='M';
+        String cuenta="20314456";
+        double suma=0;
+        suma= m + n; 
+        //Salida de Pantalla
+        System.out.println("**** DATOS DEL ALUMNO ****");
+        System.out.print("NUMERO DE CUENTA:"+cuenta+"\n");
+        
+    }
+}
